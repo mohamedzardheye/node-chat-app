d7e99f203b45c36065de7bff403e17e224c2e988
mohamed mohamud zardheye <mohamedzardheye@gmail.com>
2017-07-09 10:44:22 +0300

    added location

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 const publicPath = path.join(__dirname,'../public');
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 
 var app = express();
 var server = http.createServer(app);
@@ -27,13 +27,15 @@ io.on('connection',(socket) => {
     console.log('createMessage', message);
     io.emit('newMessage',generateMessage(message.from, message.text));
     callback('this is from server');
-    // socket.broadcast.emit('newMessage',{
-    //   from: message.from,
-    //     text: message.text,
-    //     createAt: new Date().getTime()
-    // });
 
 });
+
+socket.on('createLocationMessage', (coords) => {
+  io.emit('newLocationMessage',generateLocationMessage('Admin', coords.latitude, coords.longitude));
+});
+
+
+
 });
 
 app.get ('/', (req, res) =>{
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,7 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
+
 describe('generateMessage',() => {
   it('should generate correct message', () => {
     var from = 'jen';
@@ -9,5 +10,19 @@ describe('generateMessage',() => {
 
     expect(message.createdAt).toBeA('number');
     expect(message).toInclude({from, text});
+  });
+});
+
+
+describe('generateLocationMessage',() => {
+  it('should generate correct location', () => {
+    var from = 'admin';
+      var latitude='19';
+    var longitude ='15';
+    var url = 'https://www.google.com/maps?q=19,15';
+    var message = generateLocationMessage(from, latitude, longitude);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url});
   });
 });
